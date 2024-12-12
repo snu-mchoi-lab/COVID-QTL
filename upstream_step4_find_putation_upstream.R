1a9af32091b6c1d897727379e62d31204530098a
Jeongha Lee <54697767+jungha24@users.noreply.github.com>
2024-12-12 17:33:59 +0900

    Update upstream_step4_find_putation_upstream.R

--- upstream_step4_find_putation_upstream.R
+++ upstream_step4_find_putation_upstream.R
@@ -1,1 +1,12 @@
 ## 0. load required packages
+
+## 1. load fimo data
+
+
+## 2. merge with SCENIC data
+
+## 3. add ieQTL data and ieGene expression data
+
+## 4. add logistic regressoin data
+
+## 5. add eQTL data across the conditions of each ieQTL 
