d8db02d702fe6fd1ec07727830672b33188fddfc
Jeongha Lee <54697767+jungha24@users.noreply.github.com>
2024-12-12 16:50:55 +0900

    Create upstream_step2_logistic_regression_for_regulon_activity.R

---
+++ upstream_step2_logistic_regression_for_regulon_activity.R
@@ -0,0 +1,6 @@
+## 0. load required packages
+library(SCopeLoomR)
+library(SCENIC)
+
+## 1. load scenic output
+loom <- open_loom('20240703_upstream/pyscenic.output.exclude_otherCellType_2e5_for_SCope.loom')
