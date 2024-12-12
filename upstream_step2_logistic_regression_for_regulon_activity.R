2a969ff102e6e13cb4ad33803c90fff6f80a1138
Jeongha Lee <54697767+jungha24@users.noreply.github.com>
2024-12-12 17:00:50 +0900

    Update upstream_step2_logistic_regression_for_regulon_activity.R

--- upstream_step2_logistic_regression_for_regulon_activity.R
+++ upstream_step2_logistic_regression_for_regulon_activity.R
@@ -3,4 +3,17 @@ library(SCopeLoomR)
 library(SCENIC)
 
 ## 1. load scenic output
-loom <- open_loom('20240703_upstream/pyscenic.output.exclude_otherCellType_2e5_for_SCope.loom')
+loom <- open_loom('{path_to_output_directory}/{pyscenic.output}.loom')
+regulons_incidMat <- get_regulons(loom, column.attr.name='Regulons')
+regulons <- regulonsToGeneLists(regulons_incidMat)
+regulonsAUC <- get_regulons_AUC(loom, column.attr.name = 'RegulonsAUC')
+regulonsAucThresholds <- get_regulon_thresholds(loom)
+embeddings <- get_embeddings(loom)
+cellInfo <- get_cell_annotation(loom)
+cellInfo$condition <- paste0(cellInfo$Severity,cellInfo$Timepoint)
+meta <- as.data.frame(covid.2e5[[]])
+meta$cellbarcode <- rownames(meta)
+cellInfo$cellbarcode <- rownames(cellInfo)
+rownames(cellInfo) <- cellInfo$cellbarcode
+
+
